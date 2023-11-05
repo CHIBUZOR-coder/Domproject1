773715b20b827537e425ee48ba442ff749569d69
CHIBUZOR-coder <chibuzormekalam@gmail.com>
2023-11-05 14:48:44 +0100

    worked on responsiveness using media query in style.css file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function random(){
    let n = Math.random();
-    n = n * 3 + 1;
+    n = n * 4 + 1;
 return  Math.floor(n);
  }
 
 random();
+const a = random();
+console.log(a);
 
 const button = document.querySelector('#quoates');
 console.log(button);
@@ -15,14 +17,14 @@ console.log(person);
 
 const moreQuotes = [
   '"Abmhhgg"',
-  '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus totam quae impedit architecto delectus neque."',
-  '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sint iusto. The man GOD helped."',
-  '"  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam esse numquam sapiente, voluptas minus placeat ipsum. Dolore eligendi doloribus veritatis."',
-  '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sapiente! Quasi, quae? Quia modi consectetur minus in."',
+  ` "My concern is not whether GOD is on our side; my greatest concern is to be on GOD'S side, for GOD is always right."`,
+  `"I may not have yet what i want, but i will always be greatfull to GOD because my position today, is somebody's prayer point."`,
+  `"May your choise reflect your hopes not your fears"`,
+  `"He who has never learned to obey cannot be a commander"`,
 ];
 
 
-const persons = ["hahjj", "Abraham Lincon", "Chibuzor", "Micheal", "Enjoyment"];
+const persons = ["hahjj", "Abraham Lincon", "Chibuzor", "Nelson Mandela", "Aristotle"];
 
 console.log(moreQuotes);
 
